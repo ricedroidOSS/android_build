9acd0025b1d03a57a2bec3dd091002ad73f72d14
hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
2022-12-13 08:06:41 +0800

    optimize background dexopt service

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -122,6 +122,7 @@ endif
 # of compilation reason in the platform (as we have a unified, single path,
 # without exceptions).
 PRODUCT_SYSTEM_PROPERTIES += \
+    pm.dexopt.downgrade_after_inactive_days?=2 \
     pm.dexopt.post-boot?=extract \
     pm.dexopt.install?=speed-profile \
     pm.dexopt.install-fast?=skip \
@@ -129,7 +130,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     pm.dexopt.install-bulk-secondary?=verify \
     pm.dexopt.install-bulk-downgraded?=verify \
     pm.dexopt.install-bulk-secondary-downgraded?=extract \
-    pm.dexopt.bg-dexopt?=speed-profile \
+    pm.dexopt.bg-dexopt?=speed \
     pm.dexopt.ab-ota?=speed-profile \
     pm.dexopt.inactive?=verify \
     pm.dexopt.cmdline?=verify \
